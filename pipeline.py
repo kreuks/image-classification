c9dcc727a18e96d58581d404cfd046d74c4baa9a
bram <kreuks27@gmail.com>
2017-08-08 20:13:21 +0700

    Test run

--- pipeline.py
+++ pipeline.py
@@ -14,7 +14,7 @@ class Pipelines(object):
 
     def main(self):
         x = tf.placeholder(tf.float32, shape=[None, 28, 28, 1], name='inputs')
-        y_actual = tf.placeholder(tf.float32, shape=[None, 10], name='labels')
+        y_actual = tf.placeholder(tf.float32, shape=[None, 2], name='labels')
         is_training = tf.placeholder(tf.bool, name='is_training')
         image_generator = ImageGenerator(config=config)
         image_batch_train, label_batch_train, image_batch_test, label_batch_test = image_generator.flow_directory(
@@ -23,7 +23,7 @@ class Pipelines(object):
 
         logits = self.model(
             x,
-            num_classes=10,
+            num_classes=2,
             is_training=is_training,
             dropout_keep_prob=0.5,
             scope='NielsenNet'
@@ -37,30 +37,33 @@ class Pipelines(object):
         loss_summary = tf.summary.scalar('loss', cross_entropy)
         accuracy_summary = tf.summary.scalar('accuracy', accuracy)
 
-        eval_data = {
-            x: image_generator.resize_mnist_images(image_generator.mnist.validation.images),
-            y_actual: image_generator.mnist.validation.labels,
-            is_training: False
-        }
-
         init = tf.global_variables_initializer()
         with tf.Session() as sess:
+            sess.run(init)
             coord = tf.train.Coordinator()
             threads = tf.train.start_queue_runners(coord=coord)
 
-            sess.run(init)
             train_writer = tf.summary.FileWriter('logs/nielsen-net', sess.graph)
 
             for i in xrange(self.num_epoch):
-                images, labels = image_generator.mnist.train.next_batch(100)
+                # images, labels = image_generator.mnist.train.next_batch(100)
+                image_batch_train = image_batch_train.eval()
+                # print image_batch_train.shape
                 summary, _ = sess.run(
                     [loss_summary, train_step],
-                    feed_dict={x: ImageGenerator(config).resize_mnist_images(images), y_actual: labels, is_training: True}
+                    feed_dict={x: image_batch_train, y_actual: label_batch_train.eval(), is_training: True}
                 )
                 train_writer.add_summary(summary, i)
 
                 if i % 100 == 0:
-                    summary, acc = sess.run([accuracy_summary, accuracy], feed_dict=eval_data)
+                    summary, acc = sess.run(
+                        [accuracy_summary, accuracy],
+                        feed_dict={
+                            x: image_batch_test,
+                            y_actual: label_batch_test.eval(),
+                            is_training: False
+                        }
+                    )
                     train_writer.add_summary(summary, i)
                     print("Step: %5d, Validation Accuracy = %5.2f%%" % (i, acc * 100))
 
